1943763f6c2a27301bedff2ec231f8e023a17a4b
lemon-fuqiang <86046329@qq.com>
2022-08-19 21:58:43 +0800

    测试 修改

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
+      测试修改
     </div>
     <router-view/>
   </div>
